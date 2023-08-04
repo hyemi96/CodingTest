22c86fd35b54d534b11d014f08d02d32148ad411
송혜미 <74815320+hyemi96@users.noreply.github.com>
2023-08-04 14:13:41 +0900

    [unrated] Title: 대소문자 바꿔서 출력하기, Time: 166.39 ms, Memory: 65.3 MB -BaekjoonHub

--- 프로그래머스/unrated/181949. 대소문자 바꿔서 출력하기/대소문자 바꿔서 출력하기.java
+++ 프로그래머스/unrated/181949. 대소문자 바꿔서 출력하기/대소문자 바꿔서 출력하기.java
@@ -1,14 +1,12 @@
-//import java.util.Scanner;
 import java.util.*;
 
 public class Solution {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         String a = sc.next();
-        String[] splitter = a.split("|");
+        String[] splitter = a.split("|"); //문자 |를 검색하여 문자하나씩 구분되도록했는데 다른방법이 있을것 같다.
 
-        for (int i=0; i < splitter.length; i++) {
-            String str =  splitter[i];            
+        for (String str : splitter) {           
             //소문자면 대문자로
             if(str.equals(str.toLowerCase())){
                 System.out.print(str.toUpperCase());
